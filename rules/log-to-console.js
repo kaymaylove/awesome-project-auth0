5957068605d0d2b1eb137f9625655144872b24d6
Karen P <port.karen@gmail.com>
2019-06-06 11:18:18 -0400

    jpdate

--- rules/log-to-console.js
+++ rules/log-to-console.js
@@ -1,4 +1,5 @@
 function (user, context, callback) {
   console.log(JSON.stringify({ user: user, context: context }, null, 2));
+  console.log('testing the webhook');
   callback(null, user, context);
 }
